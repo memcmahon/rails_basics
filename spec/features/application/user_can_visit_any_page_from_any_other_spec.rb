6bc913b9d286dc611d9a5a58063041ea85bb7e9e
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-07 14:11:32 -0700

    Adds feature tests for nav bar

---
+++ spec/features/application/user_can_visit_any_page_from_any_other_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+describe "user navigation" do
+  describe "they can link to students_path and new from any page" do
+    it "they visit students_path" do
+      visit students_path
+
+      click_on("Student Index")
+
+      expect(current_path).to eq(students_path)
+
+      visit students_path
+
+      click_on("New Student")
+
+      expect(current_path).to eq(new_student_path)
+    end
+
+    it "they visit student_path" do
+      Student.create!(name: "Sirius Black")
+      visit student_path(student)
+
+      click_on("Student Index")
+
+      expect(current_path).to eq(students_path)
+
+      visit student_path(student)
+
+      click_on("New Student")
+
+      expect(current_path).to eq(new_student_path)
+    end
+
+    it "they visit edit_student_path" do
+      Student.create!(name: "Sirius Black")
+      visit edit_student_path(student)
+
+      click_on("Student Index")
+
+      expect(current_path).to eq(students_path)
+
+      visit edit_student_path(student)
+
+      click_on("New Student")
+
+      expect(current_path).to eq(new_student_path)
+    end
+
+    it "they visit new_student_path" do
+      Student.create!(name: "Sirius Black")
+      visit new_student_path(student)
+
+      click_on("Student Index")
+
+      expect(current_path).to eq(students_path)
+
+      visit new_student_path(student)
+
+      click_on("New Student")
+
+      expect(current_path).to eq(new_student_path)
+    end
+  end
+end
