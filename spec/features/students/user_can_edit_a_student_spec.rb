5222545584fde00df058c31e3ba7fd3f86ddb14b
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-07 13:52:33 -0700

    Adds ability for user to edit students

--- spec/features/students/user_can_edit_a_student_spec.rb
+++ spec/features/students/user_can_edit_a_student_spec.rb
@@ -3,13 +3,16 @@ require 'rails_helper'
 describe "user can edit an existing student" do
   describe "they visit edit_student_path" do
     it "they can edit the student name" do
-      student = Student.create!(name: "James Potter")
-      visit edit_student_path(student)
+      student_1 = Student.create!(name: "James Potter")
+      student_2 = Student.create!(name: "Remus Lupin")
+      visit edit_student_path(student_1)
 
       fill_in("student[name]", with: "Jimmy Potter")
+      click_on("Update Student")
 
       expect(page).to have_content("Jimmy Potter")
       expect(page).to_not have_content("James")
+      expect(page).to_not have_content("Remus")
     end
   end
 end
