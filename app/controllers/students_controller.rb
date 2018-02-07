3ca5d349b666140e5ce463cc646a966d4fa86781
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-07 13:44:39 -0700

    Adds ability for user to create a student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,16 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new()
+  end
+
+  def create
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   private
 
   def student_params
